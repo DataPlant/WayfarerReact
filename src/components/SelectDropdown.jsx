b702119fd9373fdcc2a5f7780acadb5a77b2b19e
DataPlant <dataplant@outlook.com>
2021-09-07 17:47:28 -0400

    select option connected

---
+++ src/components/SelectDropdown.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+class SelectDropdown extends React.Component {
+    renderData() {
+        const findCities = this.props.cities.map((cities, idx) => {
+            return (
+                    <option value={cities._id} key={idx}>
+                        {cities.cityname}
+                    </option>
+            )
+        })
+        return findCities;
+    }
+
+    render() {
+        return(
+            <>
+                {this.renderData()}
+            </>
+        )
+    }
+}
+export default SelectDropdown;
