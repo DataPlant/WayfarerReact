14c744fd4c31acfe0891f0fdb7f94624b60db6e3
off2mxrs <off2mxrs@gmail.com>
2021-09-08 12:07:09 -0700

    city component on mainpage linked to showpage

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,63 +4,75 @@ import CityShowPage from './CityShowPage';
 import CityModel from '../models/CityModel';
 import Sidebar from '../components/mainPageComponents/Sidebar';
 import City from '../components/City';
+import { Link } from 'react-router-dom';
 
 class MainPage extends React.Component {
 
-    state = {
-        cities: [],
-        cityIdx: 0
+  state = {
+    cities: [],
+    cityIdx: 0
+  }
+
+  updateCityIdx = (idx) => {
+    // Anytime we update state we must call this.setState()
+    // this.setState() will tell the component to rerender.
+    this.setState({ cityIdx: idx });
+  }
+
+  componentDidMount() {
+    console.log('did mount');
+
+    CityModel.all().then((data) => {
+      this.setState({ cities: data });
+      console.log(data);
+
+    });
+  }
+
+
+  render() {
+
+    let cityNames = this.state.cities.map(city => {
+      return city.cityname
+    })
+
+    let selectedCity = this.state.cities[this.state.cityIdx]
+
+    let linkJsx = null
+
+    if (selectedCity) {
+
+     linkJsx = <Link className="navLink" to={`/about/${this.state.cities[this.state.cityIdx]._id}`}>
+        <City city={selectedCity} /> 
+      </Link>
+
+    } else {
+      linkJsx = ''
     }
 
-    updateCityIdx = (idx) => {
-        // Anytime we update state we must call this.setState()
-        // this.setState() will tell the component to rerender.
-        this.setState({ cityIdx: idx });
-      }
+    return (
+      <div className="main-page">
 
-    componentDidMount() {
-        console.log('did mount');
-        
-        CityModel.all().then((data) => {
-            this.setState({ cities: data });
+        {/* <CityListPage cities={ this.state.cities } /> */}
 
-        });
-    }
+        <Sidebar
+          cityNames={cityNames}
+          updateCityIdx={this.updateCityIdx}
+        />
 
-  
-    render() {
-
-        let cityNames = this.state.cities.map(city => {
-            return city.cityname
-          })
-
-
-        let selectedCity = this.state.cities[this.state.cityIdx] 
-    
-      return (
-        <div className="main-page">
-
-            {/* <CityListPage cities={ this.state.cities } /> */}
-
-            <Sidebar 
-            cityNames = { cityNames }
-            updateCityIdx={ this.updateCityIdx }
-            />
-
-          <main className="category-info">
+        <main className="category-info">
           <section className="category-detail">
             Learn More About...
           </section>
-            
-            { selectedCity ? <City city={ selectedCity }/> : "" }
-          </main>
+            {linkJsx}
+        </main>
 
-        </div>
-      );
-    }
+      </div>
+    );
   }
-  
-  export default MainPage;
+}
+
+export default MainPage;
 
   // 2 components:
   //    Sidebar
