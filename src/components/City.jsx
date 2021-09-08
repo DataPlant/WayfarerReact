e75029fce2aeee0bb640b0b746a50ad22e77f890
off2mxrs <off2mxrs@gmail.com>
2021-09-07 17:47:42 -0700

    added City component to mainpage

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,14 +1,11 @@
 function City(props) {
     return (
-        <div>
-            <h2>{props.cityObj.cityname}</h2>
-            <p>{props.cityObj.country}</p>
-            <img 
-                className="city-img"
-                src={props.cityObj.headerimg}
-                alt="city picture"
-            />
-
+        <div className="category-detail">
+            <div className="category-content">
+              <h2>{props.city.cityname}</h2>
+              <h3>{props.city.country}</h3>
+              <img src={ props.city.headerimg }alt="" className="category-img"/>
+            </div>
                 
         </div>
     )
