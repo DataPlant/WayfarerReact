7f52bc3fd0b4f849a5cfe9b59d02bd29f5f94cf4
xsatyax <satyasette1@gmail.com>
2021-09-09 00:07:09 -0700

    clean up

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -10,8 +10,8 @@ function City(props) {
         )
     })
     return (
-        <div className="category-detail">
-            <div className="category-content">
+        <div className="city-detail">
+            <div className="city-content">
 
                 <h2>{props.city.cityname}</h2>
                 <h3>{props.city.country}</h3>
@@ -21,7 +21,7 @@ function City(props) {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla eros, eleifend id rutrum id, dictum dictum lorem. Aliquam at enim massa. In sollicitudin elit id mi posuere, ut consequat dui accumsan. Morbi sed condimentum urna, non varius ex. Vestibulum vitae pellentesque mauris. Proin accumsan felis id neque pretium, eu hendrerit tellus blandit. Aenean id eros sit amet neque tempus dictum ut vitae leo. Proin accumsan felis id neque pretium, eu hendrerit tellus blandit. Aenean id eros sit amet neque tempus dictum ut.
                 </p>
             </div>
-            <img src={props.city.headerimg} alt="" className="category-img" />
+            <img src={props.city.headerimg} alt="" className="city-img" />
 
         </div>
     )
