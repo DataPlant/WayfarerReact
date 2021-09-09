bbf5ba730f8c946f9173640e11d34778e9d0a34d
xsatyax <satyasette1@gmail.com>
2021-09-08 18:44:54 -0700

    added image props to city post components, changed rendering format

--- src/components/CityPosts.jsx
+++ src/components/CityPosts.jsx
@@ -4,6 +4,7 @@ function CityPosts(props) {
     let postsJsx = props.city.posts.map((post, idx) => {
         return (
         <article className="recipe">
+              <img src={ post.img } alt="" className="recipe-img" />
             <div className="recipe-container">
                     <h4 className="recipe-title">{post.title}</h4>
                     <p className="recipe-p">{post.content}</p>
