757af8f16e62e4417ea564367c463ef40e64ed55
xsatyax <satyasette1@gmail.com>
2021-10-16 15:42:53 -0700

    deploy

--- src/pages/CreatePostShowPage.jsx
+++ src/pages/CreatePostShowPage.jsx
@@ -39,7 +39,7 @@ class CreatePostShowPage extends React.Component {
             cityId,
         }
         axios
-            .post('https://wayfarer-satya.herokuapp.com/post', city)
+            .post('http://localhost:4000/post', city)
             .then(() => window.location.href = "/cities")
             .catch(err => {
                 console.error(err)
