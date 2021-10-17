27566ee478638127391cd1ccb1cece159a8a0aea
xsatyax <satyasette1@gmail.com>
2021-10-16 18:07:18 -0700

    merge

--- src/components/homePageComponents/Section.jsx
+++ src/components/homePageComponents/Section.jsx
@@ -23,32 +23,19 @@ function Section() {
             <Div>
                 <h1>Mindset.</h1>
                 <p>
-<<<<<<< HEAD
                     Nulla eget dapibus risus. Pellentesque ut mi at enim condimentum tincidunt nec in elit. Integer ultrices diam a malesuada cursus. 
-=======
-                    Nulla eget dapibus risus. Pellentesque ut mi at enim condimentum tincidunt nec in elit. Integer ultrices diam a malesuada cursus. Maecenas id vulputate leo. Proin auctor egestas porttitor. Phasellus non arcu varius, rutrum erat sed, semper dui.
->>>>>>> 95f98c535da5b039daebdf27915f485e94132eae
                 </p>
             </Div>
             <Div>
                 <h1>Lifestyle.</h1>
                 <p>
-<<<<<<< HEAD
                     Nulla eget dapibus risus. Pellentesque ut mi at enim condimentum tincidunt nec in elit. Integer ultrices diam a malesuada cursus. 
-=======
-                    Nulla eget dapibus risus. Pellentesque ut mi at enim condimentum tincidunt nec in elit. Integer ultrices diam a malesuada cursus. Maecenas id vulputate leo. Proin auctor egestas porttitor. Phasellus non arcu varius, rutrum erat sed, semper dui. Aenean est dolor,
->>>>>>> 95f98c535da5b039daebdf27915f485e94132eae
                 </p>
             </Div>
             <Div>
                 <h1>Motivator.</h1>
                 <p>
-<<<<<<< HEAD
                     Nulla eget dapibus risus. Pellentesque ut mi at enim condimentum tincidunt nec in elit. Integer ultrices diam a malesuada cursus. 
-=======
-                    Nulla eget dapibus risus. Pellentesque ut mi at enim condimentum tincidunt nec in elit. Integer ultrices diam a malesuada cursus. Maecenas id vulputate leo. Proin auctor egestas porttitor. Phasellus non arcu varius, rutrum erat sed, semper dui. Aenean est dolor,
-
->>>>>>> 95f98c535da5b039daebdf27915f485e94132eae
                 </p>
             </Div>
         </Container>
